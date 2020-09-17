3d0db5e58a0ca0fb65e50f15b986cbcbac32ac92
lanarhoades-source <67766715+lanarhoades-source@users.noreply.github.com>
2020-09-17 17:50:06 -0400

    Update pyconverter.py

--- pyconverter.py
+++ pyconverter.py
@@ -19,7 +19,7 @@ with open(py2) as f2, open(py3) as f3:
     for line in f2.readlines():
         if "print" in line:
             start_point = line.index('print')+5
-            new_line = line[start_point:]+'(' line[:start_point+1]
+            new_line = line[start_point:]+'(' +line[:start_point+1]+')'
             f3.write(new_line)
         else:
             f3.write(line)
