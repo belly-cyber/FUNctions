939398aafc4fecf323c5c24a69a650ac34a30427
miguel ball <miguel.balli69@gmail.com>
2020-09-17 17:46:25 -0400

    v2>v3

---
+++ pyconverter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+#initial convertion form 2 > 3
+#discrepencies done:
+#    print function differnce 
+#
+#future fixes:
+#    when socket use convert strings to bytes 
+#    ???
+#    ???
+
+import sys
+
+py2 = sys.argv[1]
+py3 = 'v3_'+py2
+
+with open(py2) as f2, open(py3) as f3:
+    for line in f2.readlines():
+        if "print" in line:
+            start_point = line.index('print')+5
+            new_line = line[start_point:]+'(' line[:start_point+1]
+            f3.write(new_line)
+        else:
+            f3.write(line)
+        
+        
+
